6f71ceff1b448975d1703099f273cffa479ce6aa
ills5771 <ilyayelizarov1@gmail.com>
2019-02-15 15:01:20 -0800

    initial

---
+++ instagram/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">Insta Clone Project</div>;
+  }
+}
+
+export default App;
