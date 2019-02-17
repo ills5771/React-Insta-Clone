083e9dfff77766bd017294e48c1516a40976f0c4
ills5771 <ilyayelizarov1@gmail.com>
2019-02-16 16:27:29 -0800

    refactored PostContainer to a class component

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-class CommentSection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="comment-section">
-        <div className="comments">
-          <span>{this.props.commentUsername}</span>
-          <span>{this.props.commentText}</span>
-        </div>
+// class CommentSection extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+const CommentSection = props => {
+  return (
+    <div className="comment-section">
+      <div className="comments">
+        <span>{props.commentUsername}</span>
+        <span>{props.commentText}</span>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default CommentSection;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -8,29 +8,37 @@ const Input = styled.input`
   font-size: 14px;
 `;
 
-const PostContainer = props => {
-  return (
-    <div style={{ margin: "3% 15%" }} className="post-wrapper">
-      <div
-        style={{ border: "1px solid lightGray", width: "700px" }}
-        className="post-container"
-      >
-        <Post
-          username={props.username}
-          thumbnailUrl={props.thumbnailUrl}
-          imageUrl={props.imageUrl}
-        />
-        <LikesSection likes={props.likes} />
+class PostContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        {props.comments.map(comment => (
-          <CommentSection
-            commentUsername={comment.username}
-            commentText={comment.text}
+  render() {
+    return (
+      <div style={{ margin: "3% 15%" }} className="post-wrapper">
+        <div
+          style={{ border: "1px solid lightGray", width: "700px" }}
+          className="post-container"
+        >
+          <Post
+            username={this.props.username}
+            thumbnailUrl={this.props.thumbnailUrl}
+            imageUrl={this.props.imageUrl}
           />
-        ))}
+          <LikesSection likes={this.props.likes} />
+
+          {this.props.comments.map(comment => (
+            <CommentSection
+              commentUsername={comment.username}
+              commentText={comment.text}
+            />
+          ))}
+        </div>
+        <Input type="text" placeholder="Add a comment..." />
       </div>
-      <Input type="text" placeholder="Add a comment..." />
-    </div>
-  );
-};
+    );
+  }
+}
+
 export default PostContainer;
