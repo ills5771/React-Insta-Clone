17e5312175f7a498ab9d69f93d83db92311cbecc
ills5771 <ilyayelizarov1@gmail.com>
2019-02-20 13:43:21 -0800

    refactored ConditionalRender to closer match directions in ReadMe

--- instagram/src/components/ConditionalRender/ConditionalRender.js
+++ instagram/src/components/ConditionalRender/ConditionalRender.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-const ConditionalRender = MainPage => Login => props => {
-  if (localStorage.getItem("username")) {
-    return <MainPage />;
-  }
-  return <Login />;
-};
+const ConditionalRender = MainPage => Login =>
+  class extends React.Component {
+    state = {
+      loggedIn: false
+    };
+
+    componentDidMount = () => {
+      if (window.localStorage.getItem("username", "password")) {
+        this.setState(prevState => ({ loggedIn: !prevState.loggedIn }));
+      }
+    };
+
+    render() {
+      return this.state.loggedIn ? <MainPage /> : <Login />;
+    }
+  };
 export default ConditionalRender;
