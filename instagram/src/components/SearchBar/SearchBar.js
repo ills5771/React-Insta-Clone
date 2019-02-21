5938498ca6ad9174c9bf191162cae2eacfe8b5b9
ills5771 <ilyayelizarov1@gmail.com>
2019-02-21 12:21:54 -0800

    changed left side searchbar components to styled

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,31 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import "./SearchBar.css";
+
+const LogoDiv = styled.div`
+  display: flex;
+`;
+const LineDiv = styled.div`
+  height: 35px;
+  border-left: 1px solid black;
+  margin: 0 16px;
+`;
+const BrandLogo = styled.img`
+  width: 120px;
+  height: 40px;
+`;
 
 const SearchBar = props => {
   return (
     <div className="searchbar-wrapper">
       <div className="searchbar-container">
-        <div style={{ display: "flex" }} className="logo">
+        <LogoDiv>
           <span className="logo-img">
             <i className="fab fa-instagram fa-2x" />
           </span>
-          <div
-            style={{
-              height: "35px",
-              borderLeft: "1px solid black",
-              margin: "0 16px "
-            }}
-          />
-          <span className="logo-name">
-            <img
-              style={{ width: "120px", height: "40px" }}
+          <LineDiv />
+          <span>
+            <BrandLogo
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
               alt="cursive-logo"
             />
           </span>
-        </div>
+        </LogoDiv>
         <div className="search-input">
           <input
             onChange={props.filterSearch}
@@ -42,8 +48,6 @@ const SearchBar = props => {
           }}
           className="search-icons"
         >
-          {/* <Link to="/login">logout</Link> */}
-
           <span>
             <i className="far fa-compass fa-lg" />
           </span>
